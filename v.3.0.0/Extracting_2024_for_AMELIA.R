0fcd0f1221a64e7c35e0d38d6e6102a64d82b506
Alessandro Fusta Moro <alessandro.fustamoro@gmail.com>
2025-11-12 09:53:47 -0700

    Create Extracting_2024_for_AMELIA.R

---
+++ v.3.0.0/Extracting_2024_for_AMELIA.R
@@ -0,0 +1,5 @@
+setwd("EM-CAMS")
+# sto subsettando il 2024 dai dati di AMIN per metterli dentro lo script di AMELIA
+# poi vorrei fare anche quello di tutti i dataset
+# forse però conviene fare prima NO2 o fare tutto?
+# mmm
